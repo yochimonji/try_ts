355ddd92a53f0ef574ba5ef1181830b5ad5bc821
yochimonji <yochimonji@gmail.com>
2021-08-17 20:52:26 +0900

    add 2章

---
+++ 2.ts
@@ -0,0 +1,23 @@
+const a = "小動物";
+// a = "小動物";
+
+let b: string;
+// b = 123;
+b = "123";
+
+let title = "小説家";
+// title = 123;
+// let address;
+// let address: any;
+
+let birthYear: number | "昭和" | "平成";
+birthYear = 1980;
+console.log(birthYear);
+birthYear = '昭和';
+console.log(birthYear);
+// birthYear = null;
+
+let favoriteFood: "北極" | "冷やし味噌";
+favoriteFood = "北極";
+// favoriteFood = "冷やしタンメン";
+
